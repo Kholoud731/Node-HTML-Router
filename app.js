ba4f7e9b8829230e236a99aa6555d27eb4253cdb
Kholoud731 <>
2022-04-28 22:09:58 +0200

    final version of my simple html render router

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const adminRouter = require('./routes/home')
+const userRouter = require('./routes/users')
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/user', userRouter)
+app.use(adminRouter)
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname, './views/404.html'))
+})
+
+app.listen(3000)
